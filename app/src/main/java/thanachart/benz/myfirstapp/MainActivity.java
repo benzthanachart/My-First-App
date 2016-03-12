055757a90ac4968ed0b8617dfa8df951cdadff11
masterUNG <benzeagle@yahoo.com>
2016-03-12 14:41:20 +0700

    Get Event onClick Finished

--- app/src/main/java/thanachart/benz/myfirstapp/MainActivity.java
+++ app/src/main/java/thanachart/benz/myfirstapp/MainActivity.java
@@ -1,13 +1,49 @@
 package thanachart.benz.myfirstapp;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Bind Widget
+        bindWidget();
+
+        //Button Controller
+        buttonController();
+
+    } // Main method
+
+    private void buttonController() {
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //sound effect
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.dog);
+
+                mediaPlayer.start();
+
+            } //onClick
+        });
+
+    } //buttonController
+
+    private void bindWidget() {
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
     }
-}
+} // Main Class นี่คือคลาสหลัก
